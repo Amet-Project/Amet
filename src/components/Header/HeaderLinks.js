6a50082f2b48456999f64729cf90f1c387b4d861
Oscar Lopez <oscar.lopezl@alumnos.udg.mx>
2021-09-04 10:29:10 -0500

    Cleaned code

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -40,7 +40,7 @@ export default function HeaderLinks(props) {
               Salones
             </Link>,
             <a
-              href="https://creativetimofficial.github.io/material-kit-react/#/documentation?ref=mkr-navbar"
+              href="#"
               target="_blank"
               className={classes.dropdownLink}
             >
