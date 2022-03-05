53b7152597e43f564f24a08045896c855eb214ce
Isaac Marquez <iamr_30@hotmail.com>
2022-03-05 14:14:09 -0600

    I dont know

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ametbe250c30": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "casinoImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
