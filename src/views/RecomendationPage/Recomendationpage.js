a6bc279f1c27d5a63d2386a133b3d96be3e5a454
Oscar Lopez <oscar.lopezl@alumnos.udg.mx>
2022-04-30 12:39:18 -0500

    API call test

--- src/views/RecomendationPage/Recomendationpage.js
+++ src/views/RecomendationPage/Recomendationpage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,43 +8,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Header from "components/Header/Header.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardFooter from "components/Card/CardFooter.js";
 import { Loading } from "carbon-components-react";
 
-import styles from "assets/jss/material-kit-react/views/loginPage.js";
 import informationPageStyle from "assets/jss/material-kit-react/views/informationPage.js";
 import image from "assets/img/bg7.jpg";
 
 //Amplify Imports
 import Amplify, {Storage, API, graphqlOperation } from 'aws-amplify'
-import { listCasinosWithImage } from '../../graphql/queriesExt.js'
-import { eventoPorFecha } from '../../graphql/queriesExt.js'
 import awsExports from "../../aws-exports.js";
 Amplify.configure(awsExports);
 
 const useStyles = makeStyles(informationPageStyle);
-
-const days = [
-  'domingo',
-  'lunes',
-  'martes',
-  'miercoles',
-  'jueves',
-  'viernes',
-  'sabado',
-];
-
-//Variables
-let price = 0;
-let day;
-let startHour;
-let endHour;
 
 export default function StartEventPage(props) {
   const [cardAnimaton, setCardAnimation] = useState("cardHidden");
@@ -54,7 +29,6 @@ export default function StartEventPage(props) {
   }, 700);
   const classes = useStyles();
   const { ...rest } = props;
-  const [casinos, setCasinos] = useState([]);
 
   useEffect(() => {
     fetchCasinos()
@@ -63,48 +37,17 @@ export default function StartEventPage(props) {
   //Get the whole items
   async function fetchCasinos() {
     try {
-      const casinosData = await API.graphql(graphqlOperation(listCasinosWithImage));
-      const eventosData = await API.graphql(graphqlOperation(eventoPorFecha, {fecha: date}));
-      let eventsArray = eventosData.data.eventoPorFecha.items;
-      let venuesArray = casinosData.data.listCasinos.items;
-      
-      for (let idxCasino = 0; idxCasino < venuesArray.length; idxCasino++) {
-        if (venuesArray[idxCasino].imagenes.items.length === 0) {
-          venuesArray[idxCasino].img = '';
-        }else {
-          const key_image = venuesArray[idxCasino].imagenes.items[0].file.key;
-          //REQUESTING THE IMAGE OF THE S3 BUCKET WITH THE INFO OBTEINED OF THE CORRESPONDING CASINO
-          const img = await Storage.get(key_image, {level: 'public'});
-          venuesArray[idxCasino].img = img;
-        }
-      }
-      let indexVenueToDelete = -1;
-
-      const dateMod = date.slice(6) + "-" + date.slice(3, 5)+ "-" + date.slice(0, 2) + " 00:00:00";
-      const dayNumber = new Date(dateMod).getDay();
-      day = days[dayNumber];
-      console.log('Casinos: ', venuesArray);
-      console.log('Eventos: ', eventsArray);
-
-      for (let i = 0; i < eventsArray.length; i++) {
-        for (let j = 0; j < venuesArray.length; j++) {
-          if (eventsArray[i].casino.id_casino === venuesArray[j].id) {
-            indexVenueToDelete = j;
-            break;
-          }      
-        }
-        if(indexVenueToDelete !== -1){
-          venuesArray.splice(indexVenueToDelete, 1);
-        }
-      }
-      console.log('Casinos: ', venuesArray);
-      setCasinos(venuesArray);
+      const idAuth = window.sessionStorage.getItem('idAuth');
+      const baseURL= "https://gorest.co.in/public/v2";
+      axios.get(`${baseURL}/users`).then(response =>{
+        console.log('response: ', response.data);
+      });
       window.setTimeout(function(){
 
         // Move to a new location or you can do something else
-        window.location.href = "/recomendationcheckout="+date;
+        //window.location.href = "/recomendationcheckout="+date;
 
-    }, 5000);
+    }, 10000);
     } catch (err) { console.log('error cargando casinos', err) }
   }
 
